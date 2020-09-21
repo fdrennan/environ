9090411fe79ed1fce003c892637e91f2c7318551
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-21 08:31:26 -0600

    update

--- main.R
+++ main.R
@@ -1,9 +1,18 @@
-renv::activate()
-library(reticulate)
-library(tidymodels)
-library(biggr)
-library(ndexssh)
+# renv::activate()
+# library(reticulate)
+# library(tidymodels)
+# library(biggr)
+# library(ndexssh)
+# library(shiny)
 
 system('git clone https://github.com/fdrennan/interface.git')
-print(fs::dir_ls())
-# shiny::runApp(appDir = 'interface', port = 80)
+# print(fs::dir_ls())
+# print(.libPaths())
+# runApp(appDir = 'interface', port = 3838)
+shiny::runApp(
+  appDir = 'interface',
+  port = 3000,
+  launch.browser = FALSE,
+  host = '0.0.0.0'
+)
+# import('paramiko')
